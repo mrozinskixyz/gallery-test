e1ebf0c8710fc77cc19cdae7efd83029a57430da
mrozinskixyz <40323774+mrozinskixyz@users.noreply.github.com>
2018-06-20 12:56:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,10 @@
-$(function (){
-  
-  // Cache vars
-  var $gallery = $('.gallery'),
-      $lightbox = $('.lightbox'),
-      $figure = $('figure'),
-      $close = $('.close');
-  
-  // Dribbble API shizzz
-  var token = 'b5501bcf1ef8eca0ef89aa3982ca742556d65b1832e077019397bbe0960df317';
-  var url = 'https://api.dribbble.com/v1/shots?sort=recent&access_token=' + token;
-  
-  // Grab Dribbble popular
-  $.getJSON(url, function(data) {
+const imgContent = document.querySelectorAll('.img-content-hover');
 
-    $.each(data, function(index, shot) {
-      
-      var thumb = shot.images.teaser,
-          full = shot.images.hidpi || shot.images.normal;
-      
-      var item = $('<li class="item" data-full="'+full+'"><img src="'+thumb+'" /></li>');
-      
-      $gallery.append(item);
+function showImgContent(e) {
+  for(var i = 0; i < imgContent.length; i++) {
+    imgContent[i].style.left = e.pageX + 'px';
+    imgContent[i].style.top = e.pageY + 'px';
+  }
+};
 
-    });
-    
-    // Handle item click
-    $('.item').on('click', function() {
-      
-      var full = $(this).attr('data-full');
-      
-      toggleLightbox(full);
-      
-      console.log(full);
-          
-    });
-    
-    // Toggle lightbox
-    function toggleLightbox(url) {
-      
-      if ($lightbox.is('.open')) {
-        
-        $lightbox
-          .removeClass('open')
-          .fadeOut(200);
-        
-      } else {
-                
-        $figure.css('background-image', 'url(' + url + ')');
-        $lightbox
-          .addClass('open')
-          .fadeIn(200);
-        
-      }
-      
-    }
-    
-    // Close 
-    $lightbox.on('click', toggleLightbox);
-
-});
-  
-});
+document.addEventListener('mousemove', showImgContent);
